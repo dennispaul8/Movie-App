8213fa8de7775ada38887b8c0940d67a58ce951e
dennispaul8 <95846991+dennispaul8@users.noreply.github.com>
2022-08-13 23:23:08 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,126 +1,101 @@
-// import DefaultLayout from "../layouts/DefaultLayout";
-import { useEffect, useState } from "react";
 
+import { useEffect, useState } from 'react';
 
+let API_key = "&api_key=b02a6a803e1ffd4b310ba6dbcadf3c53";
+let base_url = "https://api.themoviedb.org/3";
+let url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+let arr = ["Popular", "Theatre", "Kids", "Drama", "Comedy"];
 
+const Home = () => {
+    const [movieData, setData] = useState([]);
+    const [url_set, setUrl] = useState(url);
+    const [search, setSearch] = useState();
 
-    let API_key="&api_key=b02a6a803e1ffd4b310ba6dbcadf3c53";
-    let base_url="https://api.themoviedb.org/3";
-    let url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
-    let arr=["Popular","Theatre","Kids","Drama","Comedy", "18+"];
+    useEffect(() => {
+        fetch(url_set)
+            .then(res => res.json())
+            .then(data => {
+            setData(data.results);
+            // console.log(data.results);
+        });
+    }, [url_set])
 
-    const Home =()=>{
+    const getData = (movieType) => {
+        if (movieType === "Popular") {
+            url = base_url + "/discover/movie?sort_by=popularity.desc" + API_key;
+        }
+        if (movieType === "Theatre") {
+            url = base_url + "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22" + API_key;
+        }
+        if (movieType === "Kids") {
+            url = base_url + "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc" + API_key;
+        }
+        if (movieType === "Drama") {
+            url = base_url + "/discover/movie?with_genres=18&primary_release_year=2014" + API_key;
+        }
+        if (movieType === "Comedy") {
+            url = base_url + "/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc" + API_key;
+        }
+        setUrl(url);
 
-        const [movies, setMovies] = useState([])
-        const [url_set,setUrl]=useState(url);
-        const [search,setSearch]=useState();
-        
-        function handleChange(e) {
-            setSearch(e.currentTarget.value)
+    }
+    let img_path="https://image.tmdb.org/t/p/w500";
 
+    const searchMovie=(evt)=>{
+        if(evt.key==="Enter")
+        {
             url=base_url+"/search/movie?api_key=b02a6a803e1ffd4b310ba6dbcadf3c53&query="+search;
-                    setUrl(url);
-                   
-
-        }
-
-        function submit() {
-            setMovies(
-              [...movies, search]
-            )
-        }
-        
-        useEffect(()=>{
-            fetch(url_set).then(res=>res.json()).then(data=>{
-                setMovies(data.results);
-                // console.log(data.results)
-            });
-        },[url_set])
-
-        
-
-        const getData=(movieType)=>{
-            if(movieType==="Popular")
-            {
-                url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
-            }
-            if(movieType==="Theatre")
-            {
-                url=base_url+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22"+API_key;
-            }
-            if(movieType==="Kids")
-            {
-                url=base_url+"/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc"+API_key;
-            }
-            if(movieType==="Drama")
-            {
-                url=base_url+"/discover/movie?with_genres=18&primary_release_year=2014"+API_key;
-            }
-            if(movieType==="Comedy")
-            {
-                url=base_url+"/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc"+API_key;
-            }
-            if(movieType==="18+")
-            {
-                url=base_url+"/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc"+API_key;
-            }
             setUrl(url);
-    
+            setSearch(" ");
         }
-
-    
-      
-        let img_path="https://image.tmdb.org/t/p/w500";
-    return(
-        
-           <main className="main1">
-                <div className="header">
-
-                     <nav>
+    }
+    return (
+        <>
+            <div className="header">
+                <nav>
                     <ul>
                         {
-                            arr.map((value,pos)=>{
-                                return(
-                                    <li key={pos}><a href="#home"  name={value} onClick={(e)=>{getData(e.target.name)}}>{value}</a></li>
+                            arr.map((value, pos) => {
+                                return (
+                                    <li key={pos}><a href="#home" key={pos} name={value} onClick={(e) => { getData(e.target.name) }}>{value}</a></li>
                                 )
                             })
                         }
-                       
+
                     </ul>
                 </nav>
-        
-            <div className="search-btn">
-                <input type="text" placeholder="Enter Movie Name" 
-                className="inputText" onKeyUp={handleChange} 
-                 />
-                 <button onClick={submit} className="btn"><i className="fas fa-search"></i></button>
-            </div>
-      
-    </div>
-                   {/* <DefaultLayout> */}
-                      <div  style={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
-                         {movies.map((item, index) => {
-
-                return (
-                    
-                    <div className="movie"  key={index}>
-                        <img src={img_path+item.poster_path} alt="poster" className="poster"/>
-                            <div className="movie-details">
-                                 <div className="box" key={item.id}>
-                                     <h4 className="title">{item.title}</h4>
-                                    <p className="rating">{item.vote_average}</p>
-                            </div>
-                        <div className="overview">
-                        <h1>overview</h1>
-                        {item.overview}
+                <form>
+                <div className="search-btn">
+                        <input type="text" placeholder="Enter Movie Name" 
+                        className="inputText" onChange={(e)=>{setSearch(e.target.value)}} 
+                         onKeyPress={searchMovie}>
+                        </input>
+                        <button type='submit' aria-label="Name"><i className="fas fa-search"></i></button>
                     </div>
-                </div>
+                </form>
             </div>
-                )
-            })}
-                        </div>     
-                    {/* </DefaultLayout> */}
-           </main>
-    );
+            <div className="container">
+
+                {movieData.map((movie) => (
+
+                    <div key={movie.id} className="movie">
+                        <img src={img_path + movie.poster_path} alt="poster" className="poster" />
+                        <div className="movie-details">
+                            <div className="box">
+                                <h4 className="title">{movie.title}</h4>
+                                <p className="rating">{movie.vote_average}</p>
+                            </div>
+                            <div className="overview">
+                                <h1>overview</h1>
+                                {movie.overview}
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+
+            </div>
+        </>
+    )
 }
 export default Home;
